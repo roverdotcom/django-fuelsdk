6c71e828eada278a485f48634394d6b8e97f677a
Johannas Heller <johann@phyfus.com>
2014-02-06 14:24:49 -0800

    Added the Test and Debug client variants.

--- django_fuelsdk/fuel.py
+++ django_fuelsdk/fuel.py
@@ -1,14 +1,34 @@
+from django.core import mail
+
 from FuelSDK import ET_TriggeredSend
 
 from client import ConfigurableET_Client
 
 
-class FuelClient(object):
+class FuelApiError(StandardError):
+    pass
+
+
+class FuelClientBase(object):
+    def __init__(self):
+        raise NotImplemented()
+
+    def send(self, email, to, data):
+        raise NotImplemented()
+
+
+class FuelClient(FuelClientBase):
     def __init__(self, *args, **kwargs):
         self.client = ConfigurableET_Client(*args, **kwargs)
 
     def build_attributes(self, data):
         return [{'Name': key, 'Value': value} for key, value in data.items()]
+
+    def process_result(self, response):
+        if response.message != 'OK':
+            raise FuelApiError('API error: %s' % response.results)
+
+        return response
 
     def send(self, email, to, data):
         ts = ET_TriggeredSend()
@@ -19,4 +39,24 @@ class FuelClient(object):
             'SubscriberKey': to,
             'Attributes': self.build_attributes(data),
         }]
-        return ts.send()
+        return self.process_result(ts.send())
+
+
+class DebugFuelClient(FuelClientBase):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def send(self, email, to, data):
+        print 'Email %s sent to %s with the data %s' % (email, to, data)
+
+
+class TestFuelClient(FuelClientBase):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def send(self, email, to, data):
+        mail.outbox.append({
+            'email': email,
+            'to': to,
+            'data': data,
+        })
